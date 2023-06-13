c1c70707531078dabeadb9ad451d0224dad97685
zcchen222 <zcchen222@gmail.com>
2023-06-14 00:49:24 +0800

    merge

--- hello.cpp
+++ hello.cpp
@@ -2,11 +2,7 @@
 using namespace std; 
 
 int main() { 
-<<<<<<< HEAD
     cout << "ted! World!\n"; 
-=======
-    cout << "Hello! !\n"; 
->>>>>>> aec9f3bbfd910356852247fa8eb1963cf16a1723
     cout << "哈囉! C++!\n";
 
 
